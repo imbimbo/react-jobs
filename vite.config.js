47ebf2516ba9cce12e0d818d6488be3c9f109e6c
felipe imbimbo <f_imbimbo@hotmail.com>
2024-07-26 00:07:42 -0300

    fix: links fixed

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), nodePolyfills({  exclude: [
+    'util', // Excludes the polyfill for `http` and `node:http`.
+  ],}),],
   server: {
     port: 3000,
   },
